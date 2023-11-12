8524e2b04978ef543c46eb240fea7ed7d1628956
Zachariah Dellimore <party14534@gmail.com>
2023-11-12 11:56:34 -0500

    finished the hw

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# cmsc508-fa2023-hw7-template
+# Welcome to Homework 7
+
+## Title
+Homework 7 for CMSC508 - Database Normalization
+
+## Project Description
+This project is intended to be used as practice for creating, designing, and querying mysql databases
+
+## How to run/build this project
+To run/build this project run the following command in the folder containing hw7.qmd
+"quarto render hw7.qmd"
+Quarto should then output and html file for your use.
+
+## How to use this project
+This project is intended to produce an HTML file that the user can use to view the database I created for this homework assignment
--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -17,7 +17,11 @@
 # Drops all tables.  This section should be amended as new tables are added.
 
 SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS skills;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS peopleskills;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS peopleroles;
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -28,11 +32,30 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+CREATE TABLE skills (
+    id int NOT NULL,
+    name varchar(256) NOT NULL,
+    description varchar(4096) NOT NULL,
+    tag varchar(256) NOT NULL,
+    url varchar(256),
+    time_commitment int,
+    PRIMARY KEY (id)
+);
+
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
+
+insert into skills (id, name, description, tag) values (1, "Leadership", "Worker shows good leadership skills", "Skill 1");
+insert into skills (id, name, description, tag) values (2, "Problem solving", "Worker shows good problem solving skills", "Skill 2");
+insert into skills (id, name, description, tag) values (3, "Efficiency", "Worker is efficient", "Skill 3");
+insert into skills (id, name, description, tag) values (4, "On time", "Worker always comes on time", "Skill 4");
+insert into skills (id, name, description, tag) values (5, "Kind", "Worker shows kindness in the workplace", "Skill 5");
+insert into skills (id, name, description, tag) values (6, "Helper", "Worker helps their team mates often", "Skill 6");
+insert into skills (id, name, description, tag) values (7, "Team Player", "Worker always plays for the team", "Skill 7");
+insert into skills (id, name, description, tag) values (8, "Inventor", "Worker comes up with new ideas often", "Skill 8");
 
 
 # Section 4
@@ -41,9 +64,16 @@ SET FOREIGN_KEY_CHECKS=1;
 # All other fields can default to NULL.
 
 CREATE TABLE people (
-    people_id int,
-    people_last_name varchar(256) NOT NULL,
-    PRIMARY KEY (people_id)
+    id INT NOT NULL,
+    first_name varchar(256),
+    last_name varchar(256) NOT NULL,
+    email varchar(256),
+    linkedin_url varchar(256),
+    headshot_url varchar(256),
+    discord_handle varchar(256),
+    brief_bio varchar(4096),
+    date_joined DATE NOT NULL,
+    PRIMARY KEY (id)
 );
 
 # Section 5
@@ -51,13 +81,30 @@ CREATE TABLE people (
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
-
+insert into people (id,last_name,date_joined) values (1,'Person 1',CURDATE());
+insert into people (id,last_name,date_joined) values (2,'Person 2',CURDATE());
+insert into people (id,last_name,date_joined) values (3,'Person 3',CURDATE());
+insert into people (id,last_name,date_joined) values (4,'Person 4',CURDATE());
+insert into people (id,last_name,date_joined) values (5,'Person 5',CURDATE());
+insert into people (id,last_name,date_joined) values (6,'Person 6',CURDATE());
+insert into people (id,last_name,date_joined) values (7,'Person 7',CURDATE());
+insert into people (id,last_name,date_joined) values (8,'Person 8',CURDATE());
+insert into people (id,last_name,date_joined) values (9,'Person 9',CURDATE());
+insert into people (id,last_name,date_joined) values (10,'Person 10',CURDATE());
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
 
+CREATE TABLE peopleskills (
+    skills_id int NOT NULL,
+    people_id int NOT NULL,
+    date_acquired DATE NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (people_id) REFERENCES people(id),
+    FOREIGN KEY (skills_id) REFERENCES skills(id)
+);
 
 # Section 7
 # Populate peopleskills such that:
@@ -72,26 +119,70 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 has skills 2,5,6;
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
- 
+
+insert into peopleskills (people_id,skills_id,date_acquired) values (1,1,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (1,3,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (1,6,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (2,3,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (2,4,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (2,5,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (3,1,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (3,5,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (5,3,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (5,6,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (6,2,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (6,3,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (6,4,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (7,3,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (7,5,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (7,6,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (8,1,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (8,3,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (8,5,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (8,6,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (9,2,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (9,5,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (9,6,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (10,1,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (10,4,CURDATE());
+insert into peopleskills (people_id,skills_id,date_acquired) values (10,5,CURDATE());
 
 # Section 8
 # Create roles( id, name, sort_priority )
 # sort_priority is an integer and is used to provide an order for sorting roles
 
-
+CREATE TABLE roles (
+    name varchar(256) NOT NULL,
+    sort_priority INT NOT NULL,
+    id INT NOT NULL,
+    PRIMARY KEY (id)
+);
 
 # Section 9
 # Populate roles
 # Designer, Developer, Recruit, Team Lead, Boss, Mentor
 # Sort priority is assigned numerically in the order listed above (Designer=10, Developer=20, Recruit=30, etc.)
 
-
+insert into roles (id,name,sort_priority) values (1,"Designer", 10);
+insert into roles (id,name,sort_priority) values (2,"Developer", 20);
+insert into roles (id,name,sort_priority) values (3,"Recruit", 30);
+insert into roles (id,name,sort_priority) values (4,"Team Lead", 40);
+insert into roles (id,name,sort_priority) values (5,"Boss", 50);
+insert into roles (id,name,sort_priority) values (6,"Mentor", 60);
 
 # Section 10
 # Create peopleroles( id, people_id, role_id, date_assigned )
 # None of the fields can be null.  ID can be auto_increment
 
-
+CREATE TABLE peopleroles (
+    role_id int NOT NULL,
+    people_id int NOT NULL,
+    date_assigned DATE NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (people_id) REFERENCES people(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
 
 # Section 11
 # Populate peopleroles
@@ -106,3 +197,18 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 is Developer
 # Person 10 is Developer and Designer
 
+insert into peopleroles (people_id,role_id,date_assigned) values (1,2,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (2,5,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (2,6,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (3,2,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (3,4,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (4,3,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (5,3,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (6,2,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (6,1,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (7,1,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (8,1,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (8,4,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (9,2,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (10,2,CURDATE());
+insert into peopleroles (people_id,role_id,date_assigned) values (10,1,CURDATE());
